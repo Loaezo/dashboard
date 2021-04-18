90e8372d4ebf555485e7de20a2da29b5ffc8a2e2
Luis Loaeza <luis.loaeza@hotmail.com>
2021-04-18 01:06:02 -0500

    Añadido script para random company values

--- reto1-DashboardEmpresarial/dashboards/models.py
+++ reto1-DashboardEmpresarial/dashboards/models.py
@@ -1,8 +1,12 @@
 import uuid
+import random
 
 "Django modules"
 
 from django.db import models
+
+def random_values():
+    return str(random.sample(range(0, 10000), 50))
 
 class Company(models.Model):
     """The model for the company, including name, description, how many stock it has and the current stock price"""
@@ -11,7 +15,9 @@ class Company(models.Model):
     description = models.CharField("Descripción", max_length=100)
     stock_volume = models.IntegerField("Acciones en circulación")
     stock_price = models.IntegerField("Precio por acción")
+    market_value = models.CharField("Valores a mercado", max_length=500000, default=random_values)
 
+# esta es la tabla que es la referencia a la primera, pero no será necesaria
 class Market_cap(models.Model):
     """Market capitalization for the company in a given period"""
     market_cap_id = models.AutoField(auto_created=True, default=1, primary_key=True)
